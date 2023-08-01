ee30833290c6d00c47c6d8cce621fceddefad328
tom01 <674860357@qq.com>
2023-08-02 01:59:45 +0800

    commit docs deploying files

---
+++ assets/python_pytest_further_topics_license.md.50a02edf.js
@@ -0,0 +1,21 @@
+import{_ as s,o as n,c as e,V as a}from"./chunks/framework.d3b95951.js";const d=JSON.parse('{"title":"License","description":"","frontmatter":{},"headers":[],"relativePath":"python/pytest/further_topics/license.md","filePath":"python/pytest/further_topics/license.md"}'),t={name:"python/pytest/further_topics/license.md"},o=a(`<h1 id="license" tabindex="-1">License <a class="header-anchor" href="#license" aria-label="Permalink to &quot;License&quot;">​</a></h1><p>Distributed under the terms of the MIT license, pytest is free and open source software.</p><div class="language-"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki material-theme-palenight"><code><span class="line"><span style="color:#A6ACCD;">The MIT License (MIT)</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">Copyright (c) 2004 Holger Krekel and others</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">Permission is hereby granted, free of charge, to any person obtaining a copy of</span></span>
+<span class="line"><span style="color:#A6ACCD;">this software and associated documentation files (the &quot;Software&quot;), to deal in</span></span>
+<span class="line"><span style="color:#A6ACCD;">the Software without restriction, including without limitation the rights to</span></span>
+<span class="line"><span style="color:#A6ACCD;">use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies</span></span>
+<span class="line"><span style="color:#A6ACCD;">of the Software, and to permit persons to whom the Software is furnished to do</span></span>
+<span class="line"><span style="color:#A6ACCD;">so, subject to the following conditions:</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">The above copyright notice and this permission notice shall be included in all</span></span>
+<span class="line"><span style="color:#A6ACCD;">copies or substantial portions of the Software.</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">THE SOFTWARE IS PROVIDED &quot;AS IS&quot;, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR</span></span>
+<span class="line"><span style="color:#A6ACCD;">IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,</span></span>
+<span class="line"><span style="color:#A6ACCD;">FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE</span></span>
+<span class="line"><span style="color:#A6ACCD;">AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER</span></span>
+<span class="line"><span style="color:#A6ACCD;">LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,</span></span>
+<span class="line"><span style="color:#A6ACCD;">OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE</span></span>
+<span class="line"><span style="color:#A6ACCD;">SOFTWARE.</span></span></code></pre></div>`,3),l=[o];function p(i,c,r,A,C,T){return n(),e("div",null,l)}const I=s(t,[["render",p]]);export{d as __pageData,I as default};
